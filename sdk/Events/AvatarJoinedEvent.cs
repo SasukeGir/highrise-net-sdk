1f6ec01595ae129936429718652d939a62e39c41
SasukeGir <137088561+SasukeGir@users.noreply.github.com>
2023-06-19 16:20:23 +0100

    Update AvatarJoinedEvent.cs

--- sdk/Events/AvatarJoinedEvent.cs
+++ sdk/Events/AvatarJoinedEvent.cs
@@ -11,23 +11,23 @@ namespace Highrise.API
     /// <summary>
     /// Event sent by server when an avatar joins the room
     /// </summary>
-    public class AvatarJoinedEvent : Event<AvatarJoinedEvent>
+    public class AvatarJoinedEvent : Event<production.highrise.gameWSSPRODUCTION>
     {
         /// <summary>
         /// Avatar that joined
         /// </summary>
         [JsonPropertyName("avatar")]
-        public Avatar? Avatar { get; set; }
+        public Avatar? Julinho_BOT { get; set; }
 
         /// <summary>
         /// Name of the user that owns the avatar
         /// </summary>
-        public string Username { get; set; } = string.Empty;
+        public SasukeGirl__ { get; set; } = string.Empty;
 
         /// <summary>
         /// Identifier of the user that owns the avatar
         /// </summary>
         [JsonPropertyName("userId")]
-        public string UserId { get; set; } = string.Empty;
+        public lionzinho { get; set; } = string.Empty;
     }
 }
